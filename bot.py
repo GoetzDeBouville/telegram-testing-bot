820bfb8c7635c73ae23551a181165ca3d4f6256c
GoetzDeBouville <count.of.tuscan@gmail.com>
2020-01-26 23:03:17 +0300

    Введены handler'ы dog и cat

--- bot.py
+++ bot.py
@@ -9,16 +9,19 @@
 #9 - базовая конфигурации logging
 #10 - импорт MessageHandler - обработка ботом сообщений и Filters - фильтр для выбора типов сообщений с которыми бот работает( тест, фото, видео, аудио)
 #11 - объявление функции talk_to_me, которая возвращает сообщение пользователю
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from glob import glob
 import logging
+from random import choice
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+import settings
 
 
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080', 
-'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}} # объявление константы прокси
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', #настройка logging, указываем формат лога (время и дата события, имя файла в котором произошло событие, уровень важности события, сообщение  событии)
-                    level=logging.INFO,  # уровень логирования INFO - для получения информационных сообщений уровня info, warnig и error
-										filename='bot.log' # название файла жернала logging в котором будут отражены все события
-										)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 	#?настройка logging, указываем формат лога (время и дата события, имя файла в котором произошло событие, уровень важности события, сообщение  событии)
+                    level=logging.INFO,  											#? уровень логирования INFO - для получения информационных сообщений уровня info, warnig и error
+										filename='bot.log') 						#? название файла жернала logging в котором будут отражены все события
+
 
 
 def greet_user(bot, update):
@@ -35,14 +38,28 @@ def talk_to_me(bot, update):
 	update.message.reply_text(user_text) # отправление текста пользователя в чат с пользователем и в консоль
 
 
+def send_dog_pict(bot, update):
+	dog_list = glob('img/*dog*.jp*g')
+	dog_pic = choice(dog_list)
+	bot.send_photo(chat_id=update.message.chat.id, photo=open(dog_pic, 'rb'))
+
+
+def send_cat_pict(bot, update):
+	cat_list = glob('img/*cat*.jp*g')
+	cat_pic = choice(cat_list)
+	bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, 'rb'))
+
+
 def main():
-	mybot = Updater("777086407:AAE32a5z9xeQHfJTvqHeB1fOyoxg-pmtB-o", request_kwargs=PROXY) # ключ от нашего бота, полученный от botFather //#переменной mubot дополнительно передаем PROXY как параметр
+	mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY) # ключ от нашего бота, полученный от botFather //#переменной mubot дополнительно передаем PROXY как параметр
 	
 	logging.info('Bot starting') #первое сообщение для журнала
 
 	dp = mybot.dispatcher # объявляем переменную dp для сокращения написания команд add_hadler
-	dp.add_handler(CommandHandler('start', greet_user))
-	dp.add_handler(MessageHandler(Filters.text, talk_to_me))  # add_handler для MessageHandler, устанавливаем фильтр для текста и производим вызов функции talk_to_me
+	dp.add_handler(CommandHandler('start', greet_user)) #Handler'ы выполняются в порядке расположения(вызова)
+	dp.add_handler(CommandHandler('dog', send_dog_pict))
+	dp.add_handler(CommandHandler('cat', send_cat_pict))
+	dp.add_handler(MessageHandler(Filters.text, talk_to_me))  # add_handler для MessageHandler, устанавливаем фильтр для текста и производим вызов функции talk_to_me. #!MessageHandler должен быть последним Hendler'ом
 	
 	mybot.start_polling() # активация бота для считвания сообщений
 	mybot.idle()  # mybot работает до принудительной остановки
---
+++ settings.py
@@ -0,0 +1,3 @@
+PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080', 
+'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}} # объявление константы прокси
+API_KEY = "777086407:AAE32a5z9xeQHfJTvqHeB1fOyoxg-pmtB-o"
